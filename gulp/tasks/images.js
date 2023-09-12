4327cab9c8bf6b3d8699df63920b662022baed2a
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-09-12 20:43:49 +0500

    init

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -10,16 +10,9 @@ export const images = () => {
             })
         ))
         .pipe(app.plugins.newer(app.path.build.images))
-        .pipe(webp())
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.gulp.src(app.path.src.images))
         .pipe(app.plugins.newer(app.path.build.images))
-        // .pipe(imagemin({
-        //     progressive: true,
-        //     svgoPlugins: [{ removeViewBox: false }],
-        //     interlaced: true,
-        //     optimizationLevel: 3 // 0 to 7
-        // }))
         .pipe(app.gulp.dest(app.path.build.images))
         .pipe(app.plugins.browsersync.stream());
 }
